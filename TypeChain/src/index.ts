3a16025514e9aa10506e41f7c181ef381df15f16
KangSage <ksage@knou.ac.kr>
2019-06-03 00:34:36 +0900

    add func createNewBlock()

--- TypeChain/src/index.ts
+++ TypeChain/src/index.ts
@@ -1,4 +1,5 @@
 import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
@@ -20,15 +21,15 @@ class Block {
         data: string,
         timestamp: number
     ) {
-     this.index = index;  
-     this.hash = hash;  
-     this.previousHash = previousHash;  
-     this.data = data;  
-     this.timestamp = timestamp;  
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const genesisBlock:Block = new Block(0, "2020202002020", "", "Hello", 123456); 
+const genesisBlock: Block = new Block(1, "2020202002020", "", "Hello", 123456);
 
 let blockchain: Block[] = [genesisBlock];
 
@@ -37,6 +38,16 @@ const getBlockchain = (): Block[] => blockchain;
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    return new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 console.log(blockchain);
 
